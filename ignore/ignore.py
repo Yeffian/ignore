2d64e726e3e15582caafca19437ff574881f84c0
Yeffian <aditchakraborty2009@gmail.com>
2024-06-29 17:12:07 +0800

    remove global contents as the writing isn't in file scope anymore

--- ignore/ignore.py
+++ ignore/ignore.py
@@ -28,17 +28,11 @@ def get_available_languages():
 search = 'https://raw.githubusercontent.com/github/gitignore/main/'
 params = sys.argv[1:]
 
-contents = ''
-
 
 def generate_ignore(languages):
+    contents = ''
     for lang in languages:
-        global contents
         contents += '\n' + requests.get(search + lang + '.gitignore').text
 
-
-# generate_ignore(params)
-get_available_languages()
-
-with open('../.gitignore', 'w') as file:
-    file.write(contents)
+    with open('./.gitignore', 'w') as file:
+        file.write(contents)
